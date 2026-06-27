599869d1ecaaa44515f9bd036ea3134cdb1d228a
rksvc <rksvc@qq.com>
2026-06-27 18:59:59 +0800

    switch from Biome to Oxc

--- scripts/redirect.user.js
+++ scripts/redirect.user.js
@@ -6,14 +6,14 @@
 // ==/UserScript==
 
 switch (location.host) {
-	case 'x.com':
-	case 'twitter.com':
-		location.host = 'xcancel.com'
-		break
-	case 'www.reddit.com':
-		location.host = 'redlib.perennialte.ch'
-		break
-	case 'jump.bdimg.com':
-	case 'jump2.bdimg.com':
-		location.host = 'tieba.baidu.com'
+  case 'x.com':
+  case 'twitter.com':
+    location.host = 'xcancel.com'
+    break
+  case 'www.reddit.com':
+    location.host = 'redlib.perennialte.ch'
+    break
+  case 'jump.bdimg.com':
+  case 'jump2.bdimg.com':
+    location.host = 'tieba.baidu.com'
 }
--- scripts/rss.user.js
+++ scripts/rss.user.js
@@ -7,30 +7,30 @@
 // ==/UserScript==
 
 {
-	// https://github.com/DIYgod/RSSHub-Radar/blob/1477c8cc075d8230a9617e438a6f466c6a95dbfe/src/lib/rss.ts#L66
-	const types = [
-		'application/rss+xml',
-		'application/atom+xml',
-		'application/rdf+xml',
-		'application/rss',
-		'application/atom',
-		'application/rdf',
-		'text/rss+xml',
-		'text/atom+xml',
-		'text/rdf+xml',
-		'text/rss',
-		'text/atom',
-		'text/rdf',
-		'application/feed+json',
-	]
+  // https://github.com/DIYgod/RSSHub-Radar/blob/1477c8cc075d8230a9617e438a6f466c6a95dbfe/src/lib/rss.ts#L66
+  const types = [
+    'application/rss+xml',
+    'application/atom+xml',
+    'application/rdf+xml',
+    'application/rss',
+    'application/atom',
+    'application/rdf',
+    'text/rss+xml',
+    'text/atom+xml',
+    'text/rdf+xml',
+    'text/rss',
+    'text/atom',
+    'text/rdf',
+    'application/feed+json',
+  ]
 
-	for (const elem of document.querySelectorAll('link[type]')) {
-		if (types.includes(elem.getAttribute('type'))) {
-			const href = elem.getAttribute('href')
-			if (href)
-				GM.registerMenuCommand(elem.getAttribute('title') || href, () =>
-					GM.openInTab(new URL(href, location.href).href),
-				)
-		}
-	}
+  for (const elem of document.querySelectorAll('link[type]')) {
+    if (types.includes(elem.getAttribute('type'))) {
+      const href = elem.getAttribute('href')
+      if (href)
+        GM.registerMenuCommand(elem.getAttribute('title') || href, () =>
+          GM.openInTab(new URL(href, location.href).href),
+        )
+    }
+  }
 }
