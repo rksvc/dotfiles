e75eb25d82ba4baa19830c78400542b3f43cf5af
rksvc <rksvc@qq.com>
2026-08-07 11:16:22 +0800

    update PowerShell profile

--- Profile.ps1
+++ Profile.ps1
@@ -8,7 +8,7 @@ Function Cleanup {
 Function Backup {
     $Archive = "$HOME\ecloud\backup.zip"
     Remove-Item $Archive
-    7z a -ssw -xr!cache -xr!watch_later -xr!archwsl -xr!sublime-text $Archive $HOME\scoop\persist\* $env:AppData\rsslab\rsslab.db
+    7z a -ssw -xr!cache -xr!watch_later -xr!archwsl $Archive $HOME\scoop\persist\* $env:AppData\rsslab\rsslab.db
 }
 
 Function Awake {
